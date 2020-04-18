06c4f3571fff32176426f7149ebb7cc69b971b95
Evelina Bo <50024202+evelinaabo@users.noreply.github.com>
2020-04-18 13:34:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 $('.menu-btn').on('click',function(e){e.preventDefault;$('.menu-btn').toggleClass('menu-btn-active')});$('.menu-btn').on('click',function(){$('.burger-menu').toggleClass('b-menu-active')});$('.menu-btn').on('click',function(){$('.section').toggleClass('section-active')})
+const makets=[{name:'Tatto Salon',img:'img/wrap1.png'},{name:'new',img:'img/red-bg.jpg'},{name:'new',img:'img/red-bg.jpg'},{name:'new',img:'img/red-bg.jpg'}]
+function createMaket(maket){return `
+      <div class="maket-wrap item" style="background-image:url(${maket.img})">
+      <a href="#">${maket.name}</a>
+    </div>
+    `}
+const templates=makets.map(maket=>createMaket(maket))
+const html=templates.join(' ')
+document.querySelector('.main-makets').innerHTML=html

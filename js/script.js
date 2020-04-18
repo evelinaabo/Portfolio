829dba36a85875976dca601ac0ae5cbdcb47b86c
Evelina Bo <50024202+evelinaabo@users.noreply.github.com>
2020-04-18 17:28:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 $('.menu-btn').on('click',function(e){e.preventDefault;$('.menu-btn').toggleClass('menu-btn-active')});$('.menu-btn').on('click',function(){$('.burger-menu').toggleClass('b-menu-active')});$('.menu-btn').on('click',function(){$('.section').toggleClass('section-active')})
-const makets=[{name:'Tatto Salon',img:'img/wrap1.png'},{name:'new',img:'img/red-bg.jpg'},{name:'new',img:'img/red-bg.jpg'},{name:'new',img:'img/red-bg.jpg'}]
-function createMaket(maket){return `
+
+const makets = [
+    { name: 'Tatto Salon', img: 'img/wrap1.png', link: 'tattoo/tatto.html' },
+    { name: 'new', img: 'img/red-bg.jpg', link: '#' },
+    { name: 'new', img: 'img/red-bg.jpg', link: '#' },
+    { name: 'new', img: 'img/red-bg.jpg', link: '#' }
+  ]
+ 
+  function createMaket(maket) {
+    return `
       <div class="maket-wrap item" style="background-image:url(${maket.img})">
-      <a href="#">${maket.name}</a>
+      <a href="${maket.link}">${maket.name}</a>
     </div>
-    `}
-const templates=makets.map(maket=>createMaket(maket))
-const html=templates.join(' ')
-document.querySelector('.main-makets').innerHTML=html
+    `
+  }
+ 
+ 
+ const templates = makets.map(maket => createMaket(maket))
+ const html = templates.join(' ')
+ 
+ document.querySelector('.main-makets').innerHTML = html
+  
